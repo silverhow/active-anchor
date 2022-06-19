b8f9fbc1efc7d32e6e09be2f3edcdd828baf4cca
Sergey Manchenko <hello@s-manchenko.ru>
2022-06-19 12:31:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const activeAnchor = {
+  $header: null,
+  $links: null,
+  vars: {
+    windowHeight: 0,
+    headerHeight: 0
+  },
+  updVars() {
+    this.vars.windowHeight = window.innerHeight;
+    this.vars.headerHeight = this.$header ? this.$header.offsetHeight - 90 : 0;
+  },
+  setActive( el ) {
+    if ( !el.classList.contains( 'active' ) ) {
+      this.$links.forEach( link => link.classList.remove( 'active' ) );
+      el.classList.add( 'active' );
+    }
+  },
+  init({headerClass = 'js-header', navbarClass = 'js-active-anchor' } = {}) {
+    this.$header = document.getElementById( headerClass );
+    this.$links = Array.prototype.slice.call( document.querySelectorAll( `.${navbarClass} a` ) );
+    this.updVars();
+    window.addEventListener( 'resize', event => this.updVars() );
+
+    window.addEventListener( 'scroll', event => {
+      let scrollPosition = document.documentElement.scrollTop + this.vars.headerHeight;
+      let activeFounded = false;
+
+      this.$links
+        .filter( link => link.hash.length > 1 )
+        .forEach( link => {
+          let currentSection = document.getElementById( link.hash.slice( 1 ) );
+          if ( currentSection && scrollPosition >= currentSection.offsetTop && scrollPosition < currentSection.offsetTop + currentSection.offsetHeight ) {
+            this.setActive( link );
+            activeFounded = true;
+          }
+        } );
+
+      if ( !activeFounded )
+        this.$links.forEach( link => link.classList.remove( 'active' ) );
+    } );
+
+    this.$links
+      .filter( link => link.hash.length > 1 )
+      .forEach( link => link.addEventListener( 'click', event => {
+        event.preventDefault();
+        const el = document.getElementById( link.hash.slice( 1 ) );
+        if(el)
+          document.documentElement.scrollTop = el.offsetTop - this.vars.headerHeight;
+      } ) );
+
+  }
+};
+
+export default activeAnchor;
